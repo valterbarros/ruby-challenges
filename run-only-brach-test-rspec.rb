ba8fdbe0f1f56fa5e1f04bd7486b3eae99024436
Valter Barros <valterinside@gmail.com>
2017-07-20 18:37:46 -0300

    Update run-only-brach-test-rspec.rb

--- run-only-brach-test-rspec.rb
+++ run-only-brach-test-rspec.rb
@@ -5,6 +5,5 @@
 #Checkout to your branch with tests dynamically
 
 spec_files = `git diff --name-only hotfix/\#5224857-dont-apear-mensage-that-have-contract | grep .spec`.split /\n/
-spec_files.each {|t| "#{rspec t}"}
 tests = spec_files.map {|t| "rspec #{t}"}
 tests.each{|test| system "#{test}"}
